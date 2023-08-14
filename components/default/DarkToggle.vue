59b799ef09521df57c80ce1b386172b4a5637d6b
Alexey Baikov <baikov52@gmail.com>
2023-08-14 17:15:30 +0300

    Add NuxtUI module Fixes #18 Configured eslint + autofix

--- components/default/DarkToggle.vue
+++ components/default/DarkToggle.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 const color = useColorMode()
-function toggleDark() {
+function toggleDark () {
   color.preference = color.value === 'dark' ? 'light' : 'dark'
 }
 </script>
@@ -9,35 +9,30 @@ function toggleDark() {
   <button
     aria-label="Тема"
     :title="`Choose ${color.value === 'dark' ? 'light' : 'dark'} theme`"
-    class="group link inline-block p-1 text-gray-600 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 sm:p-2"
+    class="link group inline-block p-1 text-gray-600 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 sm:p-2"
     @click="toggleDark()"
   >
     <div class="theme-icon">
       <ClientOnly>
         <template v-if="color.value === 'dark'">
-          <Icon
-            name="line-md:moon-filled"
-            class="theme-icon icon--on"
-          />
-          <Icon
-            name="line-md:moon"
-            class="theme-icon icon--off"
+          <UIcon
+            name="i-tabler-moon"
+            class="theme-icon"
           />
         </template>
         <template v-else>
-          <Icon name="line-md:sunny-outline-loop" class="theme-icon icon--on" />
-          <Icon name="line-md:sunny-outline" class="theme-icon icon--off" />
+          <UIcon name="i-tabler-sun" class="theme-icon" />
         </template>
         <template #fallback>
-          <Icon name="line-md:loading-twotone-loop" class="theme-icon" />
+          <UIcon name="i-tabler-loader" class="theme-icon" />
         </template>
       </ClientOnly>
     </div>
   </button>
 </template>
 
-<style lang="postcss" scoped>
-.theme-icon {
+<style lang="css" scoped>
+/* .theme-icon {
   @apply h-6 w-6 md:h-6 md:w-6 text-light-200;
 }
 
@@ -46,5 +41,5 @@ function toggleDark() {
 }
 .icon--on {
   @apply !hidden group-hover:!inline-block;
-}
+} */
 </style>
--- components/default/Header.vue
+++ components/default/Header.vue
@@ -3,34 +3,20 @@
 
 <template>
   <header
-    class="sticky h-16 flex top-0 z-40 border-b border-primary-600 bg-gradient-to-r from-primary-900 to-primary-700 text-light-200"
+    class="border-primary-600 from-primary-900 to-primary-700 sticky top-0 z-40 flex h-16 border-b bg-gradient-to-r text-gray-200"
   >
-    <div class="mx-auto flex items-center flex-1 max-w-7xl px-4">
+    <UContainer class="flex flex-1 items-center">
       <DefaultLogo />
-      <nav class="md:flex hidden flex-1 items-center justify-center px-4">
+      <nav class="hidden flex-1 items-center justify-center px-4 md:flex">
       <!-- <Menu /> -->
       </nav>
       <div class="flex items-center md:flex-initial">
         <DefaultDarkToggleSwipe />
         <!-- <DefaultDarkToggle /> -->
         <NuxtLink to="https://github.com/baikov/tpl-traefik" class="ml-4">
-          <Icon name="mdi:github" class="h-8 w-8" />
+          <UIcon name="i-mdi-github" class="h-8 w-8" />
         </NuxtLink>
       </div>
-    </div>
+    </UContainer>
   </header>
 </template>
-
-<style>
-.slide-fade-from-left-enter-active {
-  transition: all 0.3s ease-out;
-}
-.slide-fade-from-left-leave-active {
-  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
-}
-.slide-fade-from-left-enter-from,
-.slide-fade-from-left-leave-to {
-  transform: translateX(-150px);
-  opacity: 0;
-}
-</style>
